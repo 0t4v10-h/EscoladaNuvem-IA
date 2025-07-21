1dedbeb820cc73837814fded36419ee478322b5f
Otavio Henrique <otaviooliveirapt@hotmail.com>
2025-07-21 19:26:48 -0300

    atualizando

--- Pratica01/02-calculadora.py
+++ Pratica01/02-calculadora.py
@@ -6,8 +6,8 @@ Use as variáveis numero1 = 12 e numero2 = 14.
 O programa deve calcular a soma e exibir o resultado.
 """
 
-numero1 = 5
-numero2 = 15
+numero1 = 12
+numero2 = 14
 soma = numero1 + numero2
 
 print("A soma de", numero1, "e", numero2, "é:", soma)
