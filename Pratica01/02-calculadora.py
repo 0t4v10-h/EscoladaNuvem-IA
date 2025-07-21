f7f1bea0f165c0fdadd993861749d1b70ddb9057
Otavio Henrique <otaviooliveirapt@hotmail.com>
2025-07-21 19:23:27 -0300

    atividade calculadora

---
+++ Pratica01/02-calculadora.py
@@ -0,0 +1,13 @@
+"""
+Calculadora de Soma
+
+Desenvolva um programa que soma dois números.
+Use as variáveis numero1 = 12 e numero2 = 14. 
+O programa deve calcular a soma e exibir o resultado.
+"""
+
+numero1 = 5
+numero2 = 15
+soma = numero1 + numero2
+
+print("A soma de", numero1, "e", numero2, "é:", soma)
